1eb0098b46738ddf00a9aeeb5254feb37d13583e
Finnegan Hewitt <fbhewitt@gmail.com>
2015-02-12 14:18:16 -0700

    User can register

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,7 +6,7 @@ class RegistrationsController < ApplicationController
 
   def create
     @user = User.new(params.require(:user).permit(:email, :password, :password_confirmation))
-    if user.save
+    if @user.save
       session[:user_id] = @user.id
       redirect_to root_path
     else
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   root "events#index"
   resources :events
 
-  get '/sign-up' => 'registration#new', as: :signup
-  post '/sign-up' => 'registration#create'
-  get '/sign-in' => 'authentication#new', as: :signin
-  post '/sign-in' => 'authentication#create'
-  get '/sign-out' => 'authentication#destroy', as: :signout
+  get '/sign-up' => 'registrations#new', as: :signup
+  post '/sign-up' => 'registrations#create'
+  get '/sign-in' => 'authentications#new', as: :signin
+  post '/sign-in' => 'authentications#create'
+  get '/sign-out' => 'authentications#destroy', as: :signout
 end
