12a5d10c86d148f562cb732222fc579ffe2677b7
Finnegan Hewitt <fbhewitt@gmail.com>
2015-02-12 14:31:59 -0700

    User can log in and log out. Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   get '/sign-up' => 'registrations#new', as: :signup
   post '/sign-up' => 'registrations#create'
-  get '/sign-in' => 'authentications#new', as: :signin
-  post '/sign-in' => 'authentications#create'
-  get '/sign-out' => 'authentications#destroy', as: :signout
+  get '/sign-in' => 'authentication#new', as: :signin
+  post '/sign-in' => 'authentication#create'
+  get '/sign-out' => 'authentication#destroy', as: :signout
 end
